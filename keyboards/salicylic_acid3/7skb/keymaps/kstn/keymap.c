28ab0927c84611b0c9342ffe1260bd2cd499ebbc
ttk-kstn <ttk-kstn@server.fake>
2022-01-22 17:32:51 +0900

    Introduce home-row modifiers

--- keyboards/salicylic_acid3/7skb/keymaps/kstn/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/kstn/keymap.c
@@ -29,6 +29,13 @@ enum custom_keycodes {
 #define THUMB_2 RSFT_T(KC_SPC)    // Shift when held. Space when tapped.
 #define THUMB_3 LT(_F2, KC_ENT)   // Layer-2 when held. Enter when tapped.
 
+// Keycode for home-row modifier keys.
+#define CTL_S CTL_T(KC_S)  // Control when held. S when tapped.
+#define ALT_F ALT_T(KC_F)  // ALt when held. F when tapped.
+#define ALT_J ALT_T(KC_J)  // ALt when held. J when tapped.
+#define CTL_L CTL_T(KC_L)  // Control when held. L when tapped.
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     }
@@ -42,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-     KC_LCTRL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
+     KC_LCTRL,    KC_A,   CTL_S,    KC_D,   ALT_F,    KC_G,        KC_H,   ALT_J,    KC_K,   CTL_L, KC_SCLN, KC_QUOT,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, KC_PSCR,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
