869a479187d1e4a9391272d9906678a6ed5eb633
ttk-kstn <ttk-kstn@server.fake>
2022-01-23 12:39:01 +0900

    Move far keys to home-row

--- keyboards/salicylic_acid3/7skb/keymaps/kstn/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/kstn/keymap.c
@@ -35,9 +35,21 @@ enum custom_keycodes {
 #define ALT_J ALT_T(KC_J)  // ALt when held. J when tapped.
 #define CTL_L CTL_T(KC_L)  // Control when held. L when tapped.
 
+// Ctrl-Alt-Delete for windows.
+#define CTRL_ALT_DEL LALT(LCTL(KC_DEL))
+
+// Keymap URL
+#define KEYMAP_URL "http://www.keyboard-layout-editor.com/##@@_c=%23777777%3B&=~%0A%0A%0A%0AESC%0A%0A%0A%0A%0A%60&_c=%23cccccc%3B&=!%0A%0A%0A%0AF1%0A%0A%0A%0A%0A1&=%2F@%0A%0A%0A%0AF2%0A%0A%0A%0A%0A2&=%23%0A%0A%0A%0AF3%0A%0A%0A%0A%0A3&=$%0A%0A%0A%0AF4%0A%0A%0A%0A%0A4&=%25%0A%0A%0A%0AF5%0A%0A%0A%0A%0A5&_x:1%3B&=%5E%0A%0A%0A%0AF6%0A%0A%0A%0A%0A6&=%2F&%0A%0A%0A%0AF7%0A%0A%0A%0A%0A7&=*%0A%0A%0A%0AF8%0A%0A%0A%0A%0A8&=(%0A%0A%0A%0AF9%0A%0A%0A%0A%0A9&=)%0A%0A%0A%0AF10%0A%0A%0A%0A%0A0&=%2F_%0A%0A%0A%0AF11%0A%0A%0A%0A%0A-&=+%0A%0A%0A%0AF12%0A%0A%0A%0A%0A%2F=&=%7C%0A%0A%0A%0A%0A%0A%0A%0A%0A%5C&_c=%23aaaaaa&a:7%3B&=DEL%3B&@_a:6&w:1.5%3B&=%60%0A%0A%60%0A%0AESC%0A%0A%0A%0ATAB&_c=%23cccccc%3B&=1%0A%0A1%0A%0AF1%0A%0A%0A%0AQ&=2%0A%0A2%0A%0AF2%0A%0A%0A%0AW&=3%0A%0A3%0A%0AF3%0A%0A%0A%0AE&=4%0A%0A4%0A%0AF4%0A%0A%0A%0AR&=5%0A%0A5%0A%0AF5%0A%0A%0A%0AT&_x:1%3B&=6%0A%0A6%0A%0AF6%0A%0A%0A%0AY&=7%0A%0A7%0A%0AF7%0A%0A%0A%0AU&=8%0A%0A8%0A%0AF8%0A%0A%0A%0AI&=9%0A%0A9%0A%0AF9%0A%0A%0A%0AO&=0%0A%0A0%0A%0AF10%0A%0A%0A%0AP&_a:4%3B&=%7B%0A%0A%0A%0AF11%0A%0A-%0A-%0A%0A%5B&=%7D%0A%0A%0A%0AF12%0A%0A%2F=%0A%2F=%0A%0A%5D&_c=%23aaaaaa&a:6&w:1.5%3B&=%5C%0A%0A%5C%0A%0A%0A%0A%0A%0ABS%3B&@_a:7&w:1.75%3B&=CTL&_c=%23eeeecc&a:6%3B&=%E2%A4%92%0A%0A~%0A%0A%0A%0A%0A%0AA&_a:4%3B&=%0A%0A%0A%0A%0A%0A%E2%A4%93%0A%5C%0A%0AS%0ACTL&_a:6%3B&=%E2%8D%90%0A%0A%7C%0A%0A%0A%0A%0A%0AD&_a:4&n:true%3B&=%0A%0A%0A%0A%0A%0A%E2%8D%97%0A%2F_%0A%0AF%0AALT&_c=%23cccccc&a:6%3B&=%3Ci%20class%2F='mss%20mss-Return-5'%3E%3C%2F%2Fi%3E%0A%0A-%0A%0A%0A%0A%0A%0AG&_x:1%3B&=+%0A%0A%E2%86%90%0A%0A%0A%0A%0A%0AH&_c=%23eeeecc&a:4&n:true%3B&=%0A%0A%0A%0A%0A%0A%2F=%0A%E2%86%93%0A%0AJ%0AALT&_a:6%3B&=%7B%0A%0A%E2%86%91%0A%0A%0A%0A%0A%0AK&_a:4%3B&=%0A%0A%0A%0A%0A%0A%7D%0A%E2%86%92%0A%0AL%0ACTL&_fa@:0&:0&:0&:0&:0&:0&:2&:2%3B%3B&=%2F:%0A%0A%0A%0A%0A%0A%3Ci%20class%2F='kb%20kb-Unicode-BackSpace-DeleteLeft-Big'%3E%3C%2F%2Fi%3E%0A%3Ci%20class%2F='kb%20kb-Unicode-BackSpace-DeleteLeft-Big'%3E%3C%2F%2Fi%3E%0A%0A%2F%3B&_c=%23cccccc&fa@:0&:0&:0&:0&:0&:0&:1&:1%3B%3B&=%22%0A%0A%0A%0A%0A%0A%3Ci%20class%2F='kb%20kb-Unicode-DeleteRight-Big'%3E%3C%2F%2Fi%3E%0A%3Ci%20class%2F='kb%20kb-Unicode-DeleteRight-Big'%3E%3C%2F%2Fi%3E%0A%0A'&_c=%23777777&a:7&w:2.25%3B&=ENTER%3B&@_c=%23aaaaaa&w:2.25%3B&=SHIFT&_c=%23cccccc&a:6%3B&=!%0A%0A!%0A%0A%0A%0A%0A%0AZ&=%2F@%0A%0A%2F@%0A%0A%0A%0A%0A%0AX&=%23%0A%0A%23%0A%0A%0A%0A%0A%0AC&=$%0A%0A$%0A%0A%0A%0A%0A%0AV&=%25%0A%0A%25%0A%0A%0A%0A%0A%0AB&_x:1%3B&=%5E%0A%0A%5E%0A%0A%0A%0A%0A%0AN&=%2F&%0A%0A%2F&%0A%0A%0A%0A%0A%0AM&_a:4&f:3%3B&=%3C%0A%0A%0A%0AVolUp%0A%0A*%0A*%0A%0A,&_f:3%3B&=%3E%0A%0A%0A%0AMute%0A%0A(%0A(%0A%0A.&_f:3%3B&=%3F%0A%0A%0A%0AVolDwn%0A%0A)%0A)%0A%0A%2F%2F&_c=%23aaaaaa&a:6&w:1.75%3B&=%2F_%0A%0A%2F_%0A%0A%0A%0A%0A%0ASHIFT&=+%0A%0A+%0A%0A%0A%0A%0A%0APrSc%3B&@_x:1.5&a:7%3B&=GUI&_w:1.5%3B&=ALT&_a:5&f:3&w:1.5%3B&=%0ALAYER(L)%0A%0A%0A%0A%0AESC&_a:4&f:3&w:1.25%3B&=%0A%0A%0A%0A%0A%0A%0A%3Ci%20class%2F='kb%20kb-Unicode-DeleteRight-Big'%3E%3C%2F%2Fi%3E%0A%0ASPC%0ASHIFT&_x:1&f:3&w:1.25%3B&=%0A%0A%0A%0A%0A%0A%3Ci%20class%2F='kb%20kb-Unicode-BackSpace-DeleteLeft-Big'%3E%3C%2F%2Fi%3E%0A%0A%0ASPC%0ASHIFT&_a:5&f:3&w:2%3B&=%0ALAYER(R)%0A%0A%0A%0A%0AENTER&_a:7&w:1.5%3B&=LAYER(F)&=%3Ci%20class%2F='kb%20kb-Unicode-Control-1'%3E%3C%2F%2Fi%3E%3Ci%20class%2F='kb%20kb-Unicode-Alternate-1'%3E%3C%2F%2Fi%3E%3Ci%20class%2F='kb%20kb-Unicode-DeleteRight-Big'%3E%3C%2F%2Fi%3E%0A%0A%0A%0AURL"
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        case KC_F20:
+            if (record->event.pressed) {
+                SEND_STRING(KEYMAP_URL);
+                return true;
+            }
+            break;
     }
     return true;
 }
@@ -53,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, KC_PSCR,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               KC_LGUI, KC_LALT, THUMB_0, THUMB_1,              THUMB_2, THUMB_3,           MO(_F), KC_RCTL
+               KC_LGUI, KC_LALT, THUMB_0, THUMB_1,              THUMB_2, THUMB_3,           MO(_F), CTRL_ALT_DEL
           //`---------------------------------------------|   |--------------------------------------------'
   ),
 
@@ -63,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-      _______, KC_HOME,  KC_END, KC_PGUP, KC_PGDN, XXXXXXX,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSPC, XXXXXXX, XXXXXXX,
+      _______, KC_HOME,  KC_END, KC_PGUP, KC_PGDN,  KC_ENT,     KC_PLUS,KC_EQUAL, KC_LCBR, KC_RCBR, KC_BSPC,  KC_DEL, _______,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               _______, _______, _______, _______,              KC_BSPC,  KC_ENT,          _______, _______ 
+               _______, _______, _______, _______,              KC_BSPC, _______,          _______, _______
           //`---------------------------------------------|   |--------------------------------------------'
   ),
 
@@ -77,11 +89,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-      _______, KC_HOME,  KC_END, KC_PGUP, KC_PGDN, XXXXXXX,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSPC, XXXXXXX, XXXXXXX,
+      _______, KC_TILD, KC_BSLS, KC_PIPE, KC_UNDS, KC_MINS,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSPC,  KC_DEL, _______,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               _______, _______, _______,  KC_DEL,              _______, _______,          _______, _______ 
+               _______, _______, _______,  KC_DEL,              _______, _______,          _______, _______
           //`---------------------------------------------|   |--------------------------------------------'
   ),
 
@@ -95,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX
+               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX, XXXXXXX,          XXXXXXX,  KC_F20
           //`---------------------------------------------|   |--------------------------------------------'
   )
 };
