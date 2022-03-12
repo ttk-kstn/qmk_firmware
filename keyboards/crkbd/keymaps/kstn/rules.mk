7429d211723626ccd406c41924be08600a716b25
ttk-kstn <ttk-kstn@server.fake>
2022-03-12 15:08:07 +0900

    Disable OLED and mouse key

--- keyboards/crkbd/keymaps/kstn/rules.mk
+++ keyboards/crkbd/keymaps/kstn/rules.mk
@@ -1,5 +1,5 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
+MOUSEKEY_ENABLE = no     # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
+OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
