e5b7b2a8c4c17d7bd5a76a83f32ed33920bf3a75
ttk-kstn <ttk-kstn@server.fake>
2022-04-03 15:55:26 +0900

    Set default LED color

--- keyboards/crkbd/keymaps/kstn/config.h
+++ keyboards/crkbd/keymaps/kstn/config.h
@@ -47,6 +47,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_DEFAULT_HUE 240
+    #define RGBLIGHT_DEFAULT_SAT 127
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
