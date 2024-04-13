e590e2f1d8cbf25c13401e2dd5b240b5712a44c7
ttk-kstn <ttk-kstn@server.fake>
2024-04-13 13:28:55 +0900

    Add my "kstn" keymap to piantor

---
+++ keyboards/beekeeb/piantor/keymaps/kstn/config.h
@@ -0,0 +1,10 @@
+// Copyright 2022 beekeeb
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Use uf2-split-left and uf2-split-right to comple.
+#define EE_HANDS
+
+// My preference.
+#define TAPPING_TERM 200
